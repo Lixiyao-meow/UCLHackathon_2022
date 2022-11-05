124f3ce2bc25f40a920f1fa6ceec30310122bebc
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-11-05 15:12:59 +0000

    Gather availability data
    convert availability to dictionary
    Add .gitignore
    Updated to support .env files
    fix data.py
    create basic flask server
    update .gitignore
    create function to gather availability data

---
+++ app.py
@@ -0,0 +1,26 @@
+from data import get_availability
+from dotenv import load_dotenv
+import os
+from flask import Flask, redirect, request
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+token =os.getenv('API_TOKEN')
+server_url = "http://localhost:5000"
+
+@app.route('/')
+def main():
+    return redirect(f"{server_url}/data")
+
+@app.route('/data')
+def uclapi_login():
+    availability = get_availability()
+    return availability
+
+if __name__ == '__main__':
+    app.run()
--- data.py
+++ data.py
@@ -1,31 +1,41 @@
 import requests
-import json
+import os
+from dotenv import load_dotenv
+from datetime import datetime
 
-client_id = '1123971576237433.9217843484076964'
-client_secret = 'e70be4f4d030498d25cd9213c0674bf99527159f4c069cb02a7b71c1917e6157'
-token ='uclapi-user-437e15c80c1f21c-0f4ca853f6a4fea-310a3d71f35d9ff-f5296842eb78eb4'
+load_dotenv()
+
+now = datetime.now()
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+token = os.getenv('API_TOKEN')
 
 
-params = {
-    "token": token,
-    "client_secret": client_secret,
-}
-r = requests.get("https://uclapi.com/workspaces/surveys", params=params)
-loc_list = r.json()['surveys']
-print(loc_list)
-data = []
-for loc in loc_list:
+def get_availability() -> dict:
     params = {
         "token": token,
         "client_secret": client_secret,
-        "survey_id": id,
     }
-    r = requests.get("https://uclapi.com/workspaces/sensors", params=params).json()
-    ids = loc['id']
-    name = loc['name']
-    print()
-    for id in ids:
+    r = requests.get("https://uclapi.com/workspaces/surveys", params=params)
+    loc_list = r.json()['surveys']
+    availability = {}
+    for loc in loc_list:
+        params = {
+            "token": token,
+            "client_secret": client_secret,
+            "survey_id": loc['id'],
+        }
+        r = requests.get("https://uclapi.com/workspaces/sensors", params=params).json()
+        counter = 0
+        length = 0
+        for floor in r['maps']:
+            for sensor in floor['sensors'].values():
+                if sensor.get('occupied', None) is None:
+                    continue
+                length += 1
+                if sensor.get('occupied', None) == True:
+                    counter += 1
+        availability[loc['id']] = counter/length
+    return availability
 
-        print(r)
-        break
-    data.append([name,])
